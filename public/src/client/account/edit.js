c239cf3dfcffb20e740539065aa61eef8eeabca1
rkinny <118126084+rkinny@users.noreply.github.com>
2025-01-23 10:46:34 -0500

    refactored code to avoid nesting functions deeper than four levels

--- public/src/client/account/edit.js
+++ public/src/client/account/edit.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 define('forum/account/edit', [
 	'forum/account/header',
@@ -14,7 +14,11 @@ define('forum/account/edit', [
 
 	AccountEdit.init = function () {
 		header.init();
+		setupEventHandlers();
+		handleEmailChange();
+	};
 
+	function setupEventHandlers() {
 		$('#submitBtn').on('click', updateProfile);
 
 		if (ajaxify.data.groupTitleArray.length === 1 && ajaxify.data.groupTitleArray[0] === '') {
@@ -23,97 +27,98 @@ define('forum/account/edit', [
 
 		handleAccountDelete();
 		handleEmailConfirm();
-		updateSignature();
-		updateAboutMe();
+		setupCharCountHandlers();
 		handleGroupControls();
+	}
 
+	function handleEmailChange() {
 		if (!ajaxify.data.isSelf && ajaxify.data.canEdit) {
 			$(`a[href="${config.relative_path}/user/${ajaxify.data.userslug}/edit/email"]`).on('click', () => {
 				changeEmail.init({
 					uid: ajaxify.data.uid,
 					email: ajaxify.data.email,
-					onSuccess: function () {
-						alerts.success('[[user:email-updated]]');
-					},
+					onSuccess: () => alerts.success('[[user:email-updated]]'),
 				});
 				return false;
 			});
 		}
-	};
+	}
 
 	function updateProfile() {
-		function getGroupSelection() {
-			const els = $('[component="group/badge/list"] [component="group/badge/item"][data-selected="true"]');
-			return els.map((i, el) => $(el).attr('data-value')).get();
-		}
+		const userData = gatherProfileData();
+		updateUserProfile(userData);
+		return false;
+	}
 
+	function gatherProfileData() {
 		const userData = $('form[component="profile/edit/form"]').serializeObject();
 		userData.uid = ajaxify.data.uid;
-		userData.groupTitle = userData.groupTitle || '';
 		userData.groupTitle = JSON.stringify(getGroupSelection());
+		return userData;
+	}
 
+	function getGroupSelection() {
+		return $('[component="group/badge/list"] [component="group/badge/item"][data-selected="true"]')
+			.map((i, el) => $(el).attr('data-value')).get();
+	}
+
+	function updateUserProfile(userData) {
 		hooks.fire('action:profile.update', userData);
 
-		api.put('/users/' + userData.uid, userData).then((res) => {
+		api.put(`/users/${userData.uid}`, userData).then((res) => {
 			alerts.success('[[user:profile-update-success]]');
 
 			if (res.picture) {
 				$('#user-current-picture').attr('src', res.picture);
+				picture.updateHeader(res.picture);
 			}
-
-			picture.updateHeader(res.picture);
 		}).catch(alerts.error);
-
-		return false;
 	}
 
-
-
 	function handleAccountDelete() {
-		$('#deleteAccountBtn').on('click', function () {
-			translator.translate('[[user:delete-account-confirm]]', function (translated) {
-				const modal = bootbox.confirm(translated + '<p><input type="password" class="form-control" id="confirm-password" /></p>', function (confirm) {
-					if (!confirm) {
-						return;
-					}
-
-					const confirmBtn = modal.find('.btn-primary');
-					confirmBtn.html('<i class="fa fa-spinner fa-spin"></i>');
-					confirmBtn.prop('disabled', true);
-					api.del(`/users/${ajaxify.data.uid}/account`, {
-						password: $('#confirm-password').val(),
-					}, function (err) {
-						function restoreButton() {
-							translator.translate('[[modules:bootbox.confirm]]', function (confirmText) {
-								confirmBtn.text(confirmText);
-								confirmBtn.prop('disabled', false);
-							});
-						}
-
-						if (err) {
-							restoreButton();
-							return alerts.error(err);
-						}
-
-						confirmBtn.html('<i class="fa fa-check"></i>');
-						window.location.href = `${config.relative_path}/`;
-					});
-
-					return false;
-				});
-
-				modal.on('shown.bs.modal', function () {
-					modal.find('input').focus();
-				});
+		$('#deleteAccountBtn').on('click', () => {
+			translator.translate('[[user:delete-account-confirm]]', (translated) => {
+				showDeleteAccountModal(translated);
 			});
 			return false;
 		});
 	}
 
+	function showDeleteAccountModal(translated) {
+		const modal = bootbox.confirm(translated + '<p><input type="password" class="form-control" id="confirm-password" /></p>', (confirm) => {
+			if (!confirm) return;
+
+			const confirmBtn = modal.find('.btn-primary');
+			confirmBtn.html('<i class="fa fa-spinner fa-spin"></i>').prop('disabled', true);
+
+			api.del(`/users/${ajaxify.data.uid}/account`, {
+				password: $('#confirm-password').val(),
+			}, (err) => {
+				if (err) {
+					restoreDeleteButton(confirmBtn);
+					return alerts.error(err);
+				}
+
+				confirmBtn.html('<i class="fa fa-check"></i>');
+				window.location.href = `${config.relative_path}/`;
+			});
+		});
+
+		modal.on('shown.bs.modal', () => {
+			modal.find('input').focus();
+		});
+	}
+
+	function restoreDeleteButton(confirmBtn) {
+		translator.translate('[[modules:bootbox.confirm]]', (confirmText) => {
+			confirmBtn.text(confirmText).prop('disabled', false);
+		});
+	}
+
 	function handleEmailConfirm() {
-		$('#confirm-email').on('click', function () {
-			const btn = $(this).attr('disabled', true);
-			socket.emit('user.emailConfirm', {}, function (err) {
+		$('#confirm-email').on('click', () => {
+			const btn = $('#confirm-email').attr('disabled', true);
+			socket.emit('user.emailConfirm', {}, (err) => {
 				btn.removeAttr('disabled');
 				if (err) {
 					return alerts.error(err);
@@ -123,56 +128,56 @@ define('forum/account/edit', [
 		});
 	}
 
+	function setupCharCountHandlers() {
+		setupCharCount('#signature', '#signatureCharCountLeft', ajaxify.data.maximumSignatureLength);
+		setupCharCount('#aboutme', '#aboutMeCharCountLeft', ajaxify.data.maximumAboutMeLength);
+	}
+
+	function setupCharCount(inputSelector, countSelector, maxLength) {
+		const el = $(inputSelector);
+		$(countSelector).html(getCharsLeft(el, maxLength));
+
+		el.on('keyup change', () => {
+			$(countSelector).html(getCharsLeft(el, maxLength));
+		});
+	}
+
 	function getCharsLeft(el, max) {
-		return el.length ? '(' + el.val().length + '/' + max + ')' : '';
-	}
-
-	function updateSignature() {
-		const el = $('#signature');
-		$('#signatureCharCountLeft').html(getCharsLeft(el, ajaxify.data.maximumSignatureLength));
-
-		el.on('keyup change', function () {
-			$('#signatureCharCountLeft').html(getCharsLeft(el, ajaxify.data.maximumSignatureLength));
-		});
-	}
-
-	function updateAboutMe() {
-		const el = $('#aboutme');
-		$('#aboutMeCharCountLeft').html(getCharsLeft(el, ajaxify.data.maximumAboutMeLength));
-
-		el.on('keyup change', function () {
-			$('#aboutMeCharCountLeft').html(getCharsLeft(el, ajaxify.data.maximumAboutMeLength));
-		});
+		return el.length ? `(${el.val().length}/${max})` : '';
 	}
 
 	function handleGroupControls() {
 		const { allowMultipleBadges } = ajaxify.data;
-		$('[component="group/toggle/hide"]').on('click', function () {
-			const groupEl = $(this).parents('[component="group/badge/item"]');
-			groupEl.attr('data-selected', 'false');
-			$(this).addClass('hidden');
-			groupEl.find('[component="group/toggle/show"]').removeClass('hidden');
-		});
 
-		$('[component="group/toggle/show"]').on('click', function () {
-			if (!allowMultipleBadges) {
-				$('[component="group/badge/list"] [component="group/toggle/show"]').removeClass('hidden');
-				$('[component="group/badge/list"] [component="group/toggle/hide"]').addClass('hidden');
-				$('[component="group/badge/list"] [component="group/badge/item"]').attr('data-selected', 'false');
+		setupGroupToggle('[component="group/toggle/hide"]', 'false', true);
+		setupGroupToggle('[component="group/toggle/show"]', 'true', false, allowMultipleBadges);
+
+		setupGroupReordering('[component="group/order/up"]', 'before');
+		setupGroupReordering('[component="group/order/down"]', 'after');
+	}
+
+	function setupGroupToggle(selector, dataSelected, hidden, allowMultipleBadges = true) {
+		$(selector).on('click', function () {
+			if (!allowMultipleBadges && dataSelected === 'true') {
+				resetGroupSelection();
 			}
 			const groupEl = $(this).parents('[component="group/badge/item"]');
-			groupEl.attr('data-selected', 'true');
-			$(this).addClass('hidden');
-			groupEl.find('[component="group/toggle/hide"]').removeClass('hidden');
+			groupEl.attr('data-selected', dataSelected);
+			$(this).toggleClass('hidden', hidden);
+			groupEl.find(selector === '[component="group/toggle/hide"]' ? '[component="group/toggle/show"]' : '[component="group/toggle/hide"]').toggleClass('hidden', !hidden);
 		});
+	}
 
-		$('[component="group/order/up"]').on('click', function () {
+	function resetGroupSelection() {
+		$('[component="group/badge/list"] [component="group/toggle/show"]').removeClass('hidden');
+		$('[component="group/badge/list"] [component="group/toggle/hide"]').addClass('hidden');
+		$('[component="group/badge/list"] [component="group/badge/item"]').attr('data-selected', 'false');
+	}
+
+	function setupGroupReordering(selector, position) {
+		$(selector).on('click', function () {
 			const el = $(this).parents('[component="group/badge/item"]');
-			el.insertBefore(el.prev());
-		});
-		$('[component="group/order/down"]').on('click', function () {
-			const el = $(this).parents('[component="group/badge/item"]');
-			el.insertAfter(el.next());
+			el[`insert${position.charAt(0).toUpperCase() + position.slice(1)}`](el[position]());
 		});
 	}
 
